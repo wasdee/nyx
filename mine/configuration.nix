8cb053ee05e8a6d988f48ae34d806e9e7136d2e6
Nuthanon Ninyawee <ninyawee@hey.com>
2025-05-19 23:19:43 +0700

    feat: enhance NVIDIA configuration for hybrid graphics support and on-the-go specialization

--- mine/configuration.nix
+++ mine/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’). 
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 # let
 #   nix-software-center = import (pkgs.fetchFromGitHub {
@@ -58,6 +58,22 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;  # see the note above
+  # https://wiki.nixos.org/wiki/NVIDIA#Disabling
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    #amdgpuBusId = "PCI:54:0:0"; # If you have an AMD iGPU
+  };
+  specialisation.on-the-go.configuration = {
+    system.nixos.tags = [ "on-the-go" ];
+    hardware.nvidia.prime = {
+      offload = {
+        enable = lib.mkForce true;
+        enableOffloadCmd = lib.mkForce true;
+      };
+      sync.enable = lib.mkForce false;    
+    };
+  };
   # TODO: https://wiki.nixos.org/wiki/CUDA
 
 
